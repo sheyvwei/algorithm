3d4cb584a367d396e2f818043c0dd5ba2379dc5d
sheyvwei <sheyvwei@gmail.com>
2019-09-25 22:04:26 +0800

    ?????

---
+++ 342.power-of-four/IsPowerOfFour.py
@@ -0,0 +1,17 @@
+'''
+
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+给定一个整数（带符号的32位），编写一个函数以检查其是否为4的幂。
+范例1： 输入16 输出：真 范例2： 输入5 输出：假
+跟进：您能否解决而无需循环/递归
+????
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+Follow up: Could you solve it without loops/recursion?
+'''
